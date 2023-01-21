4d55071888719c533c5cc68308d151cf52d09902
kwenox25 <113370944+kwenox25@users.noreply.github.com>
2023-01-20 21:45:46 -0500

    updated JS

--- js/main.js
+++ js/main.js
@@ -8,14 +8,66 @@ document referes to the index.htnl poage; querySelctor is HOW JS  uses CSS to se
 */
 
 //step #1 - select the elements you want the user to interact with
-let bitmapGraphic = document.querySelector("#bitmapGraphic");
+let Acordion = document.querySelector("#Acordion");
+let Cymbals = document.querySelector("#Cymbals");
+let ElectricGuitar = document.querySelector("#ElectricGuitar");
+let Flute = document.querySelector("#Flute");
+let Guitar = document.querySelector("#Guitar");
+let Microphone = document.querySelector("#Microphone");
+let SnareDrums = document.querySelector("#SnareDrums");
+let MusicRecord = document.querySelector("#MusicRecord");
+let Piano = document.querySelector("#Piano");
+let Xylophone = document.querySelector("#Xylophone");
 
 // step #3 - what happens when events are triggered?
 
+// function logID() {
+//     console.log(this.id);
+// }
 function logID() {
-    console.log(this.id);
+    alert("You have chosen Acordion!");
+}
+function Cymbal() {
+    alert("You have chosen Cymbals!");
+}
+function ElectricGuitars() {
+    alert("You have chosen Electric Guitar!");
+}
+function Flutes() {
+    alert("You have chosen Flute!");
+}
+function Guitars() {
+    alert("You have chosen Guitar!");
+}
+function Microphones() {
+    alert("You have chosen Microphone!");
+}
+function SnareDrum() {
+    alert("You have chosen Snare Drums!");
+}
+function MusicRecords() {
+    alert("You have chosen Music Record!");
+}
+function Pianos() {
+    alert("You have chosen Music Piano!");
+}
+function Xylophones() {
+    alert("You have chosen Xylophone!");
 }
 
 // step #2 - how do you want the user to interact with the element?
 
-bitmapGraphic.addEventListener('click', logID);
+Acordion.addEventListener('click', logID);
+Cymbals.addEventListener('click', Cymbal);
+ElectricGuitar.addEventListener('click', ElectricGuitars);
+Flute.addEventListener('click', Flutes);
+Guitar.addEventListener('click', Guitars);
+Microphone.addEventListener('click', Microphones);
+SnareDrums.addEventListener('click', SnareDrum);
+MusicRecord.addEventListener('click', MusicRecords);
+Piano.addEventListener('click', Pianos);
+Xylophone.addEventListener('click', Xylophones);
+
+
+
+
